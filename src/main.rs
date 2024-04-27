cb543f29afe124ee4f80611307bf3bf7ef5e9d58
gg <gggiulio77@gmail.com>
2024-04-27 15:04:40 -0300

    feat: add example

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
+
+use grpc_proto::helloworld::greeter_server::{Greeter, GreeterServer};
+use grpc_proto::helloworld::{HelloRequest, HelloResponse};
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = grpc_proto::helloworld::HelloResponse {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
